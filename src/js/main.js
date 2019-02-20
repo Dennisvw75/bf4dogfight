3c253222e0f97c14eb91f15b85bc7e7b00a17d91
Dennis van Wetering <dvanwetering@gmail.com>
2019-02-20 12:44:11 +0100

    Trying to solve some responsive issues...

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
-const btn = document.getElementById('btn');
-const planesShow = document.getElementById('planes');
-const planesItem = document.querySelectorAll('.planes__item');
+const current = document.querySelector('#current');
+const imgs = document.querySelector('.imgs');
+const img = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
 
-btn.addEventListener('click', function() {
-  planesShow.style.display = 'block';
-});
+img[0].style.opacity = opacity;
+
+imgs.addEventListener('click', imgClick);
+
+function imgClick(e) {
+  img.forEach(img => (img.style.opacity = 1));
+
+  current.src = e.target.src;
+
+  current.classList.add('fade-in');
+
+  setTimeout(() => current.classList.remove('fade-in'), 500);
+
+  e.target.style.opacity = opacity;
+}
